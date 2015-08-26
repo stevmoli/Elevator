b752a127fb3049a63a4e5e5c069d22b5d1989ae3
stevmoli <stevmoli@users.noreply.github.com>
2015-08-26 00:05:03 -0400

    Animation spacing working for minutes

--- src/main.c
+++ src/main.c
@@ -5,10 +5,8 @@
 Window *my_window;
 GBitmap *zero, *one, *two, *three, *four, *five, *six, *seven, *eight, *nine;
 TextLayer *text_hours_layer;
-TextLayer *text_minutes_layer;
 TextLayer *text_colon_layer;
 TextLayer *text_long_hours_layer;
-TextLayer *text_short_minutes_layer;
 static GFont HBH_font;
 TextLayer *background_layer;
 
@@ -111,17 +109,17 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     long_hours_offset = 10;
   }
   
-  if (strncmp("1", &time_buffer[2], 1) == 0) { // handles tens place of minutes when it is 1
-    long_minutes_offset = 81; 
-      if (strncmp("1", &time_buffer[3], 1) == 0) { // handles ones place of minutes
-        short_minutes_offset = long_minutes_offset + 7;
+  if (strncmp("1", &time_buffer[3], 1) == 0) { // handles tens place of minutes when it is 1
+    long_minutes_offset = 72; 
+      if (strncmp("1", &time_buffer[4], 1) == 0) { // handles ones place of minutes
+        short_minutes_offset = long_minutes_offset + 16;
       } else {
         short_minutes_offset = long_minutes_offset + 24;  // GOOD
       }
   } else { // handles tens place of minutes when it is NOT 1
     long_minutes_offset = 81;
-      if (strncmp("1", &time_buffer[3], 1) == 0) { // handles ones place of minutes
-        short_minutes_offset = long_minutes_offset + 28;
+      if (strncmp("1", &time_buffer[4], 1) == 0) { // handles ones place of minutes
+        short_minutes_offset = long_minutes_offset + 24;
       } else {
         short_minutes_offset = long_minutes_offset + 32;
       }
@@ -247,31 +245,43 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   // Add one time animations to fix initial positioning when watchface starts
   if (format_fix == 1) {
     if (long_hours_offset == 22) { // for if the one's place of the hour is a one when the face launches
-      GRect digit_start = GRect (11, 20, 34, 140);
-      GRect digit_finish = GRect (long_hours_offset, 20, 34, 140);
-      animate_digit_layer(text_layer_get_layer(text_long_hours_layer), &digit_start, &digit_finish, 100, 1);
+      GRect digit_start = GRect (4, 8, 26, 149);
+      GRect digit_finish = GRect (long_hours_offset, 8, 26, 149);
+      animate_digit_layer(bitmap_layer_get_layer(lnghr), &digit_start, &digit_finish, 1000, 1);
     }
     if (long_hours_offset == 21) {
       GRect digit_start = GRect (11, 20, 34, 140);
       GRect digit_finish = GRect (long_hours_offset, 20, 34, 140);
-      animate_digit_layer(text_layer_get_layer(text_long_hours_layer), &digit_start, &digit_finish, 100, 1);
+      animate_digit_layer(text_layer_get_layer(text_long_hours_layer), &digit_start, &digit_finish, 1000, 1);
     }
     if (long_hours_offset == 10) {
       GRect digit_start = GRect (11, 20, 34, 140);
       GRect digit_finish = GRect (long_hours_offset, 20, 34, 140);
-      animate_digit_layer(text_layer_get_layer(text_long_hours_layer), &digit_start, &digit_finish, 1, 1);
+      animate_digit_layer(text_layer_get_layer(text_long_hours_layer), &digit_start, &digit_finish, 1000, 1);
     }
     if (short_hours_offset == 9) {
       GRect digit_start = GRect(10, 20, 57, 140);
       GRect digit_finish = GRect(short_hours_offset, 20, 57, 140);
-      animate_digit_layer(text_layer_get_layer(text_hours_layer), &digit_start, &digit_finish, 1, 1);
+      animate_digit_layer(text_layer_get_layer(text_hours_layer), &digit_start, &digit_finish, 1000, 1);
     }
-    // NEED LONG_HOURS_OFFSET CASE HERE FOR IF MINUTES TENS IS A 1 *************************
-    if (short_minutes_offset == 88) {
-      GRect digit_start = GRect (99, 20, 33, 140);
-      GRect digit_finish = GRect (short_minutes_offset, 20, 33, 140);
-      animate_digit_layer(text_layer_get_layer(text_short_minutes_layer), &digit_start, &digit_finish, 100, 1);
+    if (strncmp("1", &time_buffer[3], 1) == 0) { // for if the tens place of the minute is a one when the face launches
+      GRect digit_start = GRect(81, 8, 26, 149);
+      GRect digit_finish = GRect(long_minutes_offset, 8, 26, 149);
+      animate_digit_layer(bitmap_layer_get_layer(lngmn), &digit_start, &digit_finish, 1000, 1);
+      
+      GRect digit_start2 = GRect(113, 8, 26, 149);  // the ones place has to be moved accordingly too
+      GRect digit_finish2 = GRect(short_minutes_offset, 8, 26, 149);
+      animate_digit_layer(bitmap_layer_get_layer(srtmn), &digit_start2, &digit_finish2, 1000, 1);
+    } else if (strncmp("1", &time_buffer[4], 1) == 0) { // for if the ones place of the minute is a one when the face launches
+      GRect digit_start = GRect(113, 8, 26, 149);
+      GRect digit_finish = GRect(short_minutes_offset, 8, 26, 149);
+      animate_digit_layer(bitmap_layer_get_layer(lngmn), &digit_start, &digit_finish, 1000, 1);
     }
+//     if (short_minutes_offset == 88) {
+//       GRect digit_start = GRect (99, 20, 33, 140);
+//       GRect digit_finish = GRect (short_minutes_offset, 20, 33, 140);
+//       animate_digit_layer(text_layer_get_layer(text_short_minutes_layer), &digit_start, &digit_finish, 100, 1);
+//     }
     format_fix--; 
   }
   
@@ -325,12 +335,6 @@ void window_load (Window *my_window) {
   text_layer_set_text_alignment(text_hours_layer, GTextAlignmentRight);
   text_layer_set_font(text_hours_layer, HBH_font);
   
-  text_minutes_layer = text_layer_create(GRect(77, 20, 66, 140));
-  text_layer_set_background_color(text_minutes_layer, GColorClear);
-  text_layer_set_text_color(text_minutes_layer, GColorWhite);
-  text_layer_set_text_alignment(text_minutes_layer, GTextAlignmentLeft);
-  text_layer_set_font(text_minutes_layer, HBH_font);
-  
   text_colon_layer = text_layer_create(GRect(1, 10, 143, 120));
   text_layer_set_background_color(text_colon_layer, GColorClear);
   text_layer_set_text_color(text_colon_layer, GColorWhite);
@@ -344,30 +348,17 @@ void window_load (Window *my_window) {
   text_layer_set_text_alignment(text_long_hours_layer, GTextAlignmentRight);
   text_layer_set_font(text_long_hours_layer, HBH_font);
   
-  text_short_minutes_layer = text_layer_create(GRect(short_minutes_offset, 20, 33, 140));
-  text_layer_set_background_color(text_short_minutes_layer, GColorClear);
-  text_layer_set_text_color(text_short_minutes_layer, GColorWhite);
-  text_layer_set_text_alignment(text_short_minutes_layer, GTextAlignmentLeft);
-  text_layer_set_font(text_short_minutes_layer, HBH_font);
-  
   
   //loading the layers
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(background_layer));
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_hours_layer));
-  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_minutes_layer));
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_long_hours_layer));
-  layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_short_minutes_layer));
   layer_add_child(window_get_root_layer(my_window), text_layer_get_layer(text_colon_layer));
   
   layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(lnghr));
   layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(srthr));
   layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(lngmn));
   layer_add_child(window_get_root_layer(my_window), bitmap_layer_get_layer(srtmn));
-  
-  //bitmap_layer_set_bitmap(lnghr, eight); // delete these tests!!!!!
-  //bitmap_layer_set_bitmap(srthr, nine); // delete these tests!!!!!
-  //bitmap_layer_set_bitmap(lngmn, four); // delete these tests!!!!!
-  //bitmap_layer_set_bitmap(srtmn, five); // delete these tests!!!!!
   
   
   // preventing face from starting blank
@@ -379,12 +370,15 @@ void window_load (Window *my_window) {
 }
 
 void window_unload (Window *my_window) {
+  bitmap_layer_destroy(lnghr);
+  bitmap_layer_destroy(srthr);
+  bitmap_layer_destroy(lngmn);
+  bitmap_layer_destroy(srtmn);
+  
   text_layer_destroy(text_hours_layer);
-  text_layer_destroy(text_minutes_layer);
   text_layer_destroy(background_layer);
   text_layer_destroy(text_colon_layer);
   text_layer_destroy(text_long_hours_layer);
-  text_layer_destroy(text_short_minutes_layer);
   fonts_unload_custom_font(HBH_font);
 }
 
